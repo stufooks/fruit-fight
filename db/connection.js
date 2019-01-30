d47df10cddadd2e6440330f13cd38405f4a64c1f
Stuart Fooks <sb.fooks@gmail.com>
2019-01-30 14:11:53 -0500

    fix local database name

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL)
 } else {
-    mongoose.connect("mongodb://localhost/whenpresident");
+    mongoose.connect("mongodb://localhost/fruit");
 }
 
 mongoose.Promise = Promise
